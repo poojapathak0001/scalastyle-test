ef31f3a9850e74bccfd8531dd0c11cb75499f540
Pooja Pathak <poopatha@publicisgroupe.net>
2018-05-31 12:07:37 +0530

    ghjuf

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ gcloud auth activate-service-account --key-file ../cdp-demo-d55fc085d0a7.json
 export PROJECT=$(gcloud info --format='value(config.project)')
 gsutil mb gs://cdp-demo-205604-jenkins-artifacts
 
-mvn clean install scalastyle:check org.jacoco:jacoco-maven-plugin:0.7.9:prepare-agent
+mvn clean install scalastyle:check org.jacoco:jacoco-maven-plugin:0.7.9:prepare-agent sonar:sonar
 gsutil cp /opt/bitnami/apps/jenkins/jenkins_home/workspace/pipeline-trial/target/exclude-resources-demo-0.0.1-SNAPSHOT.jar gs://cdp-demo-205604-jenkins-artifacts/$JOB_NAME/$BUILD_NUMBER/
 
 gsutil acl get gs://cdp-demo-205604-jenkins-artifacts/$JOB_NAME/$BUILD_NUMBER/exclude-resources-demo-0.0.1-SNAPSHOT.jar
